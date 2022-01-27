3c88c8c831d3ac54742d778f9e032434bb038fb5
Ash Almert <fast56511@gmail.com>
2022-01-27 23:13:37 +0300

    feat: add gulp-autoprefixer in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ const browserSync = require('browser-sync').create();
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const sass =  require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
 
- function bs() {
+function bs() {
     serveSass();
     browserSync.init({
         server: {
@@ -26,6 +27,9 @@ function minCSS() {
 function serveSass() {
     return src('./sass/*.sass')
         .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(dest('./css'))
         .pipe(browserSync.stream());
 };
